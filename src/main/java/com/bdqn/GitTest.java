619a5e70afff0d1732f0f650d4a0b4862bdddee7
wang <676913227@qq.com>
2022-07-11 16:36:34 +0800

    master second commit

---
+++ src/main/java/com/bdqn/GitTest.java
@@ -0,0 +1,12 @@
+package com.bdqn;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello........git");
+        System.out.println("hello........git");
+        System.out.println("hello........git");
+        System.out.println("hello........git");
+        System.out.println("hello........git");
+        System.out.println("hello........git");
+    }
+}
